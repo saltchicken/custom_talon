0175cdbb86883a1c32d98d115478f12de62532ac
saltchicken <john.eicher89@gmail.com>
2024-03-30 12:58:25 -0700

    Refactor

--- custom/custom.py
+++ custom/custom.py
@@ -7,24 +7,31 @@ mod = Module()
 # def say_hello(m) -> int:
 #     return m
 
-@mod.capture(rule="one|two|minimize window")
-def command_send(word) -> bool:
+def _send_command_local(command):
     try:
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_address = ('localhost', 12345)
         print('Connecting to {} port {}'.format(*server_address))
         client_socket.connect(server_address)
+        print(f'Sending {command}')
+        client_socket.sendall(command.encode('utf-8'))
     except:
         print('Connection failed')
         return False
-    try:
+    finally:
+        print('Closing socket')
+        client_socket.close() 
+    
+@mod.capture(rule="minimize window")
+def command_send(word) -> None:
         if len(word._sequence) > 1:
             command = " ".join(word._sequence)
         else:
             command = word._sequence[0]
-        print(f'Sending {command}')
-        client_socket.sendall(command.encode('utf-8'))
-    finally:
-        print('Closing socket')
-        client_socket.close() 
-    return True
+        _send_command_local(command)
+
+@mod.capture(rule="Jarvis <phrase>")
+def command_test(word) -> bool:
+    phrase = word._sequence[1]
+    print(phrase)
+    _send_command_local(phrase)
--- custom/custom.talon
+++ custom/custom.talon
@@ -1,1 +1,3 @@
 <user.command_send>: ""
+
+<user.command_test>: ""
