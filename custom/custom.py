1797d26c03ae5590ea79efb96ac027ad6797eb93
saltchicken <john.eicher89@gmail.com>
2024-04-03 19:45:10 -0700

    Use JSONfor message passing

--- custom/custom.py
+++ custom/custom.py
@@ -1,26 +1,24 @@
 from talon import Module, Context
-import socket
+import socket, json
+
+from dataclasses import dataclass
 
 mod = Module()
 
-# @mod.capture(rule="one")
-# def say_hello(m) -> int:
-#     return m
-
-def _send_command_local(command):
-    try:
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_address = ('localhost', 12345)
-        print('Connecting to {} port {}'.format(*server_address))
-        client_socket.connect(server_address)
-        print(f'Sending {command}')
-        client_socket.sendall(command.encode('utf-8'))
-    except:
-        print('Connection failed')
-        return False
-    finally:
-        print('Closing socket')
-        client_socket.close()
+# def _send_command_local(command):
+#     try:
+#         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#         server_address = ('localhost', 12345)
+#         print('Connecting to {} port {}'.format(*server_address))
+#         client_socket.connect(server_address)
+#         print(f'Sending {command}')
+#         client_socket.sendall(command.encode('utf-8'))
+#     except:
+#         print('Connection failed')
+#         return False
+#     finally:
+#         print('Closing socket')
+#         client_socket.close()
         
 def _send_command_remote(command):
     try:
@@ -28,8 +26,9 @@ def _send_command_remote(command):
         server_address = ('192.168.1.11', 12345)
         print('Connecting to {} port {}'.format(*server_address))
         client_socket.connect(server_address)
-        print(f'Sending {command}')
-        client_socket.sendall(command.encode('utf-8'))
+        data = {"type": "phrase", "message": command}
+        data_string = json.dumps(data)
+        client_socket.sendall(data_string.encode())
     except:
         print('Connection failed')
         return False
@@ -37,16 +36,16 @@ def _send_command_remote(command):
         print('Closing socket')
         client_socket.close() 
     
-@mod.capture(rule="minimize window")
-def command_send(word) -> None:
-        if len(word._sequence) > 1:
-            command = " ".join(word._sequence)
-        else:
-            command = word._sequence[0]
-        _send_command_local(command)
+# @mod.capture(rule="minimize window")
+# def command_send(word) -> None:
+#         if len(word._sequence) > 1:
+#             command = " ".join(word._sequence)
+#         else:
+#             command = word._sequence[0]
+#         _send_command_local(command)
 
 @mod.capture(rule="Jarvis <phrase>")
 def command_test(word) -> bool:
     phrase = word._sequence[1]
-    print(phrase)
-    _send_command_remote(phrase)
+    send_phrase = ' '.join(phrase.words)
+    _send_command_remote(send_phrase)
