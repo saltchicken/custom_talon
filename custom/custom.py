f13a42be1ba3ec9a7387dc7bf4b46a5c108c40d6
saltchicken <john.eicher89@gmail.com>
2024-04-07 15:36:41 -0700

    Add new commands

--- custom/custom.py
+++ custom/custom.py
@@ -6,7 +6,7 @@ mod = Module()
 def _send_command_remote(command):
     try:
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_address = ('192.168.1.11', 9999)
+        server_address = ('192.168.1.11', 8000)
         print('Connecting to {} port {}'.format(*server_address))
         client_socket.connect(server_address)
         data = {"type": "phrase", "message": command}
@@ -18,12 +18,43 @@ def _send_command_remote(command):
     finally:
         print('Closing socket')
         client_socket.close() 
+        
+def _send_command_message_remote(command):
+    try:
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_address = ('192.168.1.11', 8000)
+        print('Connecting to {} port {}'.format(*server_address))
+        client_socket.connect(server_address)
+        data = {"type": "command", "message": 'interrupt'}
+        data_string = json.dumps(data)
+        client_socket.sendall(data_string.encode())
+    except:
+        print('Connection failed')
+        return False
+    finally:
+        print('Closing socket')
+        client_socket.close() 
     
+    
+
+        
 @mod.capture(rule="Jarvis <phrase>")
 def command_send(word) -> bool:
     phrase = word._sequence[1]
-    send_phrase = ' '.join(phrase.words)
-    _send_command_remote(send_phrase)
+    # print(phrase.words[0])
+    if phrase.words[0] == 'stop':
+        _send_command_message_remote('stop')
+    elif phrase.words[0] == 'clear':
+        _send_command_message_remote('clear')
+    else:
+        send_phrase = ' '.join(phrase.words)
+        _send_command_remote(send_phrase)
+    
+# @mod.capture(rule="Jarvis stop")
+# def command_send(word) -> bool:
+#     # phrase = word._sequence[1]
+#     # send_phrase = ' '.join(phrase.words)
+#     _send_command_message_remote('stop')
     
 # @mod.capture(rule="minimize window")
 # def command_send(word) -> None:
